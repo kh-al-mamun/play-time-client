9a0eeeb7840b2bf28658dd0ec25d141e4a504429
khmamun <khmamun27@gmail.com>
2023-05-20 09:49:33 +0600

    Gallery added

---
+++ src/components/Gallery/Gallery.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+import './Gallery.css'
+import ItemDetails from '../ItemDetails/ItemDetails';
+
+const Gallery = () => {
+    const [imageData, setImageData] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:5000/animal-toys/gallery")
+            .then(response => response.json())
+            .then(data => setImageData(data))
+    }, [])
+
+    const handleItemsDetails = (id) => {
+        console.log(id)
+    }
+
+    return (
+            <div className='gallery'>
+                {
+                    imageData.map(image => {
+
+                        return (
+                            <>
+                                <div
+                                    key={image._id}
+                                    className='pics'
+                                    onClick={() => handleItemsDetails(image._id)}>
+                                    <img src={image.picture} style={{ width: '100%' }}></img>
+                                </div>
+
+                                {/* <div style={{position:'fixed', top: '0'}}>
+                                    <ItemDetails
+                                    ></ItemDetails>
+                                </div> */}
+                            </>
+
+
+                        )
+                    })
+                }
+            </div>
+
+    );
+};
+
+export default Gallery;
